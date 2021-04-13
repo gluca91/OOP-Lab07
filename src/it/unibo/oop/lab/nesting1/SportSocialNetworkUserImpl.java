86cdf7558b7b0b7588013b9361bbd09fa5a6a8b1
Gianluca Ensini <gianluca.ensini@studio.unibo.it>
2021-04-13 15:57:07 +0200

    nesting1

--- src/it/unibo/oop/lab/nesting1/SportSocialNetworkUserImpl.java
+++ src/it/unibo/oop/lab/nesting1/SportSocialNetworkUserImpl.java
@@ -19,8 +19,7 @@ import it.unibo.oop.lab.socialnetwork.User;
  * 
  * 2) Add the unimplemented methods.
  * 
- * @param <U>
- *            specific {@link User} type
+ * @param <U> specific {@link User} type
  */
 public class SportSocialNetworkUserImpl<U extends User> extends SocialNetworkUserImpl<U> {
 
@@ -53,12 +52,12 @@ public class SportSocialNetworkUserImpl<U extends User> extends SocialNetworkUse
      * TODO: initialize properly these sports
      */
     static {
-        SOCCER = null;
-        F1 = null;
-        MOTOGP = null;
-        VOLLEY = null;
-        BASKET = null;
-        BIKE = null;
+        SOCCER = new Sport("soccer");
+        F1 = new Sport("f1");
+        MOTOGP = new Sport("motoGp");
+        VOLLEY = new Sport("volley");
+        BASKET = new Sport("basket");
+        BIKE = new Sport("bike");
     }
 
     /**
@@ -69,13 +68,10 @@ public class SportSocialNetworkUserImpl<U extends User> extends SocialNetworkUse
     /**
      * Builds a new {@link SportSocialNetworkUserImpl}.
      * 
-     * @param name
-     *            the user firstname
-     * @param surname
-     *            the user lastname
-     * @param user
-     *            alias of the user, i.e. the way a user is identified on an
-     *            application
+     * @param name    the user firstname
+     * @param surname the user lastname
+     * @param user    alias of the user, i.e. the way a user is identified on an
+     *                application
      */
     public SportSocialNetworkUserImpl(final String name, final String surname, final String user) {
         this(name, surname, user, -1);
@@ -84,15 +80,11 @@ public class SportSocialNetworkUserImpl<U extends User> extends SocialNetworkUse
     /**
      * Builds a new {@link SportSocialNetworkUserImpl}.
      * 
-     * @param name
-     *            the user firstname
-     * @param surname
-     *            the user lastname
-     * @param userAge
-     *            user's age
-     * @param user
-     *            alias of the user, i.e. the way a user is identified on an
-     *            application
+     * @param name    the user firstname
+     * @param surname the user lastname
+     * @param userAge user's age
+     * @param user    alias of the user, i.e. the way a user is identified on an
+     *                application
      */
     public SportSocialNetworkUserImpl(final String name, final String surname, final String user, final int userAge) {
         super(name, surname, user, userAge);
@@ -106,27 +98,25 @@ public class SportSocialNetworkUserImpl<U extends User> extends SocialNetworkUse
      */
 
     /**
-     * Add a new sport followed by this user: if the user already likes or does
-     * the sport, nothing happens.
+     * Add a new sport followed by this user: if the user already likes or does the
+     * sport, nothing happens.
      * 
-     * @param sport
-     *            a sport followed/done by the user
+     * @param sport a sport followed/done by the user
      */
-    // TODO
     public void addSport(final Sport sport) {
-
+        if (!sports.contains(sport)) {
+            sports.add(sport);
+        }
     }
 
     /**
      * Returns true if a user likes/does a given sport.
      * 
-     * @param s
-     *            sport to use as an input
+     * @param s sport to use as an input
      * @return true if a user likes sport s
      */
-    // TODO
     public boolean hasSport(final Sport s) {
-        return false;
+        return sports.contains(s);
     }
 
     /*
@@ -136,15 +126,39 @@ public class SportSocialNetworkUserImpl<U extends User> extends SocialNetworkUse
      * with its bare name.
      */
     public static final class Sport {
+
+        private final String name;
+        private int hash;
+
+        public Sport(final String name) {
+            this.name = name;
+        }
+
         /*
          * TODO
          * 
-         * Redefine equals so that two sports are equal only if they feature the
-         * very same name. Remember that you must also redefine hashCode()!
+         * Redefine equals so that two sports are equal only if they feature the very
+         * same name. Remember that you must also redefine hashCode()!
          */
         @Override
         public boolean equals(final Object o) {
+            if (o != null && o instanceof Sport) {
+                final Sport s = (Sport) o;
+                return this.name.equals(s.getName());
+            }
             return false;
+        }
+
+        @Override
+        public int hashCode() {
+            if (hash == 0) {
+                hash = this.name.hashCode();
+            }
+            return hash;
+        }
+
+        public String getName() {
+            return this.name;
         }
     }
 }
