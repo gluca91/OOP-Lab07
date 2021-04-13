4234b6be7e49fbf1ffe592b196a70c3927501e17
Gianluca Ensini <gianluca.ensini@studio.unibo.it>
2021-04-13 15:28:50 +0200

    anonymus 1

--- src/it/unibo/oop/lab/anonymous1/TestAnonymousComparator.java
+++ src/it/unibo/oop/lab/anonymous1/TestAnonymousComparator.java
@@ -1,6 +1,8 @@
 package it.unibo.oop.lab.anonymous1;
 
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 
 import it.unibo.oop.lab.socialnetwork.SocialNetworkUser;
@@ -21,22 +23,22 @@ import it.unibo.oop.lab.socialnetwork.User;
  */
 public final class TestAnonymousComparator {
 
-    private TestAnonymousComparator() { }
+    private TestAnonymousComparator() {
+    }
 
     /**
      * Check whether the users in result list are in the same order as those in
      * expected.
      * 
-     * @param expected
-     *            the expected list of users
-     * @param result
-     *            the list of users returned by the test
+     * @param expected the expected list of users
+     * @param result   the list of users returned by the test
      * @return true is result's users are in the same order as expected's users
      */
     public static boolean checkUserOrder(final List<User> expected, final List<User> result) {
         for (int i = 0; i < expected.size(); i++) {
             if (!expected.get(i).equals(result.get(i))) {
-                System.out.println("[EXCEPTION] [POS. " + i + "] [EXPECTED] " + expected.get(i) + " [GOT] " + result.get(i));
+                System.out.println(
+                        "[EXCEPTION] [POS. " + i + "] [EXPECTED] " + expected.get(i) + " [GOT] " + result.get(i));
                 return false;
             }
         }
@@ -44,15 +46,15 @@ public final class TestAnonymousComparator {
     }
 
     /**
-     * @param args
-     *            ignored
+     * @param args ignored
      */
     public static void main(final String... args) {
         /*
          * create 6 social network users
          */
         final SocialNetworkUser<User> kbacon = new SocialNetworkUserImpl<>("Kevin", "Bacon", "kbacon", 56);
-        final SocialNetworkUser<User> dwashington = new SocialNetworkUserImpl<>("Denzel", "Washington", "dwashington", 59);
+        final SocialNetworkUser<User> dwashington = new SocialNetworkUserImpl<>("Denzel", "Washington", "dwashington",
+                59);
         final SocialNetworkUser<User> mgladwell = new SocialNetworkUserImpl<>("Malcom", "Gladwell", "mgladwell", 51);
         final SocialNetworkUser<User> ntaleb = new SocialNetworkUserImpl<>("Nicholas", "Taleb", "ntaleb", 54);
         final SocialNetworkUser<User> mrossi = new SocialNetworkUserImpl<>("Mario", "Rossi", "mrossi", 31);
@@ -69,12 +71,21 @@ public final class TestAnonymousComparator {
          * 
          * - define an anonymous comparator to sort incrementally by age
          * 
-         * NOTE: in order to sort a list think about a method of the utility
-         * class java.util.Collections
+         * NOTE: in order to sort a list think about a method of the utility class
+         * java.util.Collections
          * 
          * REFER TO LESSON 13-Advanced-Mechanisms.pdf, slide 41
          */
-        // TODO
+
+        final var myComp = new Comparator<User>() {
+            @Override
+            public int compare(User o1, User o2) {
+                return o1.getAge() - o2.getAge();
+            }
+        };
+        denzelUsers.sort(myComp);
+//        denzelUsers.sort((i, j) -> i.getAge() - j.getAge());
+
         /*
          * expected Result
          */
@@ -102,10 +113,24 @@ public final class TestAnonymousComparator {
          * 
          * - define an anonymous comparator to sort by age decreasingly
          * 
-         * NOTE: in order to sort a list think about a method of the utility
-         * class Collections
+         * NOTE: in order to sort a list think about a method of the utility class
+         * Collections
          */
-        // TODO
+        {
+//            rossiUsers.sort(Collections.reverseOrder(myComp));
+        }
+        {
+//          rossiUsers.sort(myComp.reversed());            
+        }
+        {
+//          rossiUsers.sort((i, j) -> j.getAge() - i.getAge());            
+        }
+
+        {
+            rossiUsers.sort((i, j) -> i.getAge() - j.getAge());
+            Collections.sort(rossiUsers, myComp.reversed());
+        }
+
         /*
          * expected Result
          */
